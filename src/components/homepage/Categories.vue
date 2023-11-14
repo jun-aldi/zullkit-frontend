7b8f14ebf06cd26ff30755e11dcb98ca4b296f15
juniaraldi <aldinugraha708@gmail.com>
2023-11-14 14:08:18 +0700

    first commit

---
+++ src/components/homepage/Categories.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import {ref} from 'vue'
+import CategoriesCard from '@/components/CategoriesCard.vue'
+
+const categories = ref([
+    {id:1, title: 'Mobile UI KIT', count: 731, image:'categories-1.jpg'},
+    {id:2, title: 'Font', count: 657, image:'categories-2.jpg'},
+    {id:3, title: 'Icon Set', count: 83559, image:'categories-3.jpg'},
+    {id:4, title: 'Website Ui kit', count: 74800, image:'categories-4.jpg'}
+])
+
+
+</script>
+
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">
+            Top Categories
+        </h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <CategoriesCard v-for="category in categories " 
+            :key="category.id" 
+            :title="category.title" 
+            :count="category.count" 
+            :image="category.image" />
+        </div>
+    </div>
+</template>
---
+++ src/components/homepage/Items.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from 'vue'
+import ItemsCard from '@/components/ItemsCard.vue'
+const items = ref([
+    {id:1, title: 'Mobile UI KIT', category: 'Mobile UI kit', image:'items-1.jpg'},
+    {id:2, title: 'Online Doctor Consultation', category: 'Website UI Kit', image:'items-2.jpg'},
+    {id:3, title: 'Banking Crypto', category: 'Mobile UI Kit', image:'items-3.jpg'},
+    ])
+</script>
+
+<template>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        <ItemsCard v-for="item in items " 
+            :key="item.id" 
+            :title="item.title" 
+            :category="item.category" 
+            :image="item.image" />
+    </div>
+  </div>
+</template>
