2800335e5a7ffd80e52d6d9e85bc2c452f100a46
juniaraldi <aldinugraha708@gmail.com>
2024-04-02 22:12:41 +0700

    register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,8 +1,6 @@
 <script setup>
 import RegisterForm from "@/components/authentication/RegisterForm.vue";
-import IconLayer from "../components/icons/IconLayer.vue";
-import IconBook from "../components/icons/IconBook.vue";
-import IconBadge from "../components/icons/IconBadge.vue";
+import FeatureList from "../components/authentication/features/FeatureList.vue";
 </script>
 
 <template>
@@ -21,42 +19,7 @@ import IconBadge from "../components/icons/IconBadge.vue";
           <div class="w-full p-5 mx-auto md:max-w-4xl">
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
               <div class="mx-auto md:mx-0">
-                <div class="mb-12">
-                  <div class="block mb-2">
-                    <IconLayer />
-                    <h3 class="inline-block ml-2 text-lg font-semibold">
-                      Customizable
-                    </h3>
-                  </div>
-                  <p class="mt-2 text-lg text-gray-500">
-                    Accelerate your work with our millions <br />
-                    of ready-to-use design projects.
-                  </p>
-                </div>
-                <div class="mb-12">
-                  <div class="block mb-2">
-                    <IconBadge />
-                    <h3 class="inline-block ml-2 text-lg font-semibold">
-                      500 Fortune
-                    </h3>
-                  </div>
-                  <p class="mt-2 text-lg text-gray-500">
-                    Accelerate your work with our millions <br />
-                    of ready-to-use design projects.
-                  </p>
-                </div>
-                <div class="mb-12">
-                  <div class="block mb-2">
-                    <IconBook />
-                    <h3 class="inline-block ml-2 text-lg font-semibold">
-                      Documentation
-                    </h3>
-                  </div>
-                  <p class="mt-2 text-lg text-gray-500">
-                    Accelerate your work with our millions <br />
-                    of ready-to-use design projects.
-                  </p>
-                </div>
+                <FeatureList />
               </div>
               <div>
                 <RegisterForm />
